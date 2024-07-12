0772d671a007d4c6a79a3926ab6ed8cac9e2a1c4
nelson-perscholas <nelsonchang.peru@gmail.com>
2024-07-12 12:34:44 -0400

    GLAB - 309.9.2 - AC17 - Spring Boot - Upload PDF File

---
+++ src/main/java/com/nchang/springdemo/model/FileUploadModel.java
@@ -0,0 +1,11 @@
+package com.nchang.springdemo.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileUploadModel {
+    private String uploadedFile;
+}
